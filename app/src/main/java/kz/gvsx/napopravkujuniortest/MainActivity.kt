e6c83d510bff3126da884273a523350049c515e7
dev-gvs <44653764+dev-gvs@users.noreply.github.com>
2022-01-28 22:47:20 +0600

    Update MainActivity.kt

--- app/src/main/java/kz/gvsx/napopravkujuniortest/MainActivity.kt
+++ app/src/main/java/kz/gvsx/napopravkujuniortest/MainActivity.kt
@@ -1,14 +1,7 @@
 package kz.gvsx.napopravkujuniortest
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
-    }
-}
+class MainActivity : AppCompatActivity(R.layout.main_activity)
