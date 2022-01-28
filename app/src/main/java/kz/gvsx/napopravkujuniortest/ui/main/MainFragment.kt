b77538a28617e67bf59d30edca0e80ed562e1a72
dev-gvs <44653764+dev-gvs@users.noreply.github.com>
2022-01-28 13:37:47 +0600

    Add ViewBinding

--- app/src/main/java/kz/gvsx/napopravkujuniortest/ui/main/MainFragment.kt
+++ app/src/main/java/kz/gvsx/napopravkujuniortest/ui/main/MainFragment.kt
@@ -1,21 +1,22 @@
 package kz.gvsx.napopravkujuniortest.ui.main
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import by.kirich1409.viewbindingdelegate.viewBinding
 import kz.gvsx.napopravkujuniortest.R
+import kz.gvsx.napopravkujuniortest.databinding.MainFragmentBinding
 
 class MainFragment : Fragment(R.layout.main_fragment) {
 
+    private val viewBinding: MainFragmentBinding by viewBinding()
     private lateinit var viewModel: MainViewModel
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        viewBinding.message.text = "Main Fragment Text"
     }
 
     companion object {
