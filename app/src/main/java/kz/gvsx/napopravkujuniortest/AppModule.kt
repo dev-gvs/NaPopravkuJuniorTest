ff6aa41bfbb68281b229a7dabeaac0ef839cd427
dev-gvs <44653764+dev-gvs@users.noreply.github.com>
2022-01-29 14:37:03 +0600

    Add logging of Retrofit requests

--- app/src/main/java/kz/gvsx/napopravkujuniortest/AppModule.kt
+++ app/src/main/java/kz/gvsx/napopravkujuniortest/AppModule.kt
@@ -7,16 +7,34 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import kotlinx.serialization.json.Json
 import kz.gvsx.napopravkujuniortest.data.GitHubService
-import okhttp3.MediaType
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
+import timber.log.Timber
 import javax.inject.Singleton
 
 private const val BASE_URL = "https://api.github.com/"
-private val MEDIA_TYPE = MediaType.get("application/json")
+private val MEDIA_TYPE = "application/json".toMediaType()
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        val builder = OkHttpClient.Builder()
+
+        if (BuildConfig.DEBUG)
+            builder.addInterceptor(HttpLoggingInterceptor {
+                Timber.tag("OkHttp").d(it)
+            }.apply {
+                setLevel(HttpLoggingInterceptor.Level.BASIC)
+            })
+
+        return builder.build()
+    }
 
     @Provides
     @Singleton
@@ -26,8 +44,9 @@ object AppModule {
 
     @Provides
     @Singleton
-    fun provideRetrofit(json: Json): Retrofit = Retrofit.Builder()
+    fun provideRetrofit(json: Json, client: OkHttpClient): Retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
+        .client(client)
         .addConverterFactory(json.asConverterFactory(MEDIA_TYPE))
         .build()
 
