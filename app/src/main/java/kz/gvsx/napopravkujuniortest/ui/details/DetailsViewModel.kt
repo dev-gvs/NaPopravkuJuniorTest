5da95c9019096f68d585b785a85012b7bbdb9051
dev-gvs <44653764+dev-gvs@users.noreply.github.com>
2022-01-29 16:09:18 +0600

    Get and log selected repository commits info

--- app/src/main/java/kz/gvsx/napopravkujuniortest/ui/details/DetailsViewModel.kt
+++ app/src/main/java/kz/gvsx/napopravkujuniortest/ui/details/DetailsViewModel.kt
@@ -2,13 +2,27 @@ package kz.gvsx.napopravkujuniortest.ui.details
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import kz.gvsx.napopravkujuniortest.data.GitHubService
+import kz.gvsx.napopravkujuniortest.domain.Repository
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
 class DetailsViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
+    gitHubService: GitHubService
 ) : ViewModel() {
     val selectedRepository = savedStateHandle.get<Repository>(DetailsFragment.REPOSITORY_KEY)
 
+    init {
+        selectedRepository?.let { repository ->
+            viewModelScope.launch {
+                val commits = gitHubService.listRepositoryCommits(repository.fullName)
+                Timber.d(commits.first().toString())
+            }
+        }
+    }
 }
