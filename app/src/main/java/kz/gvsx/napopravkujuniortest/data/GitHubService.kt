8f15740673bfa0dae775f35242c024c2ad3aeb6d
dev-gvs <44653764+dev-gvs@users.noreply.github.com>
2022-01-29 16:07:58 +0600

    Add method to request repository commits

--- app/src/main/java/kz/gvsx/napopravkujuniortest/data/GitHubService.kt
+++ app/src/main/java/kz/gvsx/napopravkujuniortest/data/GitHubService.kt
@@ -1,11 +1,18 @@
 package kz.gvsx.napopravkujuniortest.data
 
+import kz.gvsx.napopravkujuniortest.domain.Commit
 import kz.gvsx.napopravkujuniortest.domain.Repository
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface GitHubService {
 
     @GET("/repositories")
     suspend fun listPublicRepositories(@Query("since") since: Int): List<Repository>
+
+    @GET("/repos/{full_name}/commits")
+    suspend fun listRepositoryCommits(
+        @Path("full_name", encoded = true) fullName: String
+    ): List<Commit>
 }
