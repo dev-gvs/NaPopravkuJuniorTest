896048b07d58c8afc92677b405d80142b22393cb
dev-gvs <44653764+dev-gvs@users.noreply.github.com>
2022-02-02 13:40:06 +0600

    Add utility function to create ConcatAdapter from the PagingDataAdapter
    Similarly to withLoadStateHeaderAndFooter, but for the header use LoadType.REFRESH, so that it handles initial data load.

--- app/src/main/java/kz/gvsx/napopravkujuniortest/Utils.kt
+++ app/src/main/java/kz/gvsx/napopravkujuniortest/Utils.kt
@@ -5,9 +5,18 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.paging.LoadState
+import androidx.paging.LoadStateAdapter
+import androidx.paging.LoadType
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.ConcatAdapter
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
+/**
+ * Convert `dp` value to the `px`.
+ */
 inline val Int.px: Int get() = (this * Resources.getSystem().displayMetrics.density).toInt()
 
 /**
@@ -23,4 +32,23 @@ inline fun Fragment.launchAndRepeatWithViewLifecycle(
             block()
         }
     }
+}
+
+/**
+ * Create a [ConcatAdapter] with the provided [LoadStateAdapter]s displaying the
+ * [LoadType.REFRESH] (for initial load) and [LoadType.APPEND] [LoadState]s as
+ * list items at the start and end respectively.
+ *
+ * @see LoadStateAdapter
+ * @see PagingDataAdapter.withLoadStateHeaderAndFooter
+ */
+fun <T : Any, V : RecyclerView.ViewHolder> PagingDataAdapter<T, V>.withHeaderAndFooter(
+    header: LoadStateAdapter<*>,
+    footer: LoadStateAdapter<*>
+): ConcatAdapter {
+    addLoadStateListener { loadStates ->
+        header.loadState = loadStates.refresh
+        footer.loadState = loadStates.append
+    }
+    return ConcatAdapter(header, this, footer)
 }
